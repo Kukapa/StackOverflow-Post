c73f9ab72f9112a81fbee2c013400a8e1a200c21
maksimgornostaev <maksimgornostaev197@gmail.com>
2023-12-15 13:33:01 +0300

    add code

---
+++ StackOverflow Post/Post.cs
@@ -0,0 +1,40 @@
+﻿namespace StackOverflow
+{
+    public class Post
+    {
+        public string Title{ get; set; }
+        public string Description { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public int Vote{ get; set; }
+        private bool HasVotedUp;
+        private bool HasVotedDown;
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            TimeCreated = DateTime.Now;
+            Vote = 1;
+        }
+
+        public void VoteUp()
+        {
+            if (HasVotedUp)
+                throw new Exception("You've already up voted");
+            else
+                Vote++;
+            HasVotedUp = true;
+            HasVotedDown = false;
+        }
+
+        public void VoteDown()
+        {
+            if (HasVotedDown)
+                throw new Exception("You've already down voted");
+            else
+                Vote--;
+            HasVotedUp = true;
+            HasVotedDown = false;
+        }
+    }
+}
---
+++ StackOverflow Post/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StackOverflow
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post("Does my post work?", "Test to see if my post works.");
+            Console.WriteLine($"Title: {post.Title}");
+            Console.WriteLine($"Description: {post.Description}");
+            Console.WriteLine($"Date Created: {post.TimeCreated}");
+            Console.WriteLine($"Post Count: {post.Vote}");
+            post.VoteDown();
+            Console.WriteLine($"Post Count: {post.Vote}");
+            post.VoteUp();
+            Console.WriteLine($"Post Count: {post.Vote}");
+        }
+    }
+}
